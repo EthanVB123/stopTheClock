fe6a5a9f1afc15f0df38f1244c6e30adccea75e5
EthanVB123 <ethanvanbruchem@gmail.com>
2020-07-18 11:58:52 +0800

    Working v2

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 console.log('Connected!')
-timeRemaining = 500;
-
+var timeRemaining = 500;
+var attempts = 1;
+var maxAttempts = 5;
+var score = 0;
+var targetScore = 50;
 var clock = setInterval(tickClock, 10)
+var scoreHUD = setInterval(updateScore, 10)
+var x = 0
 function tickClock() {
-    document.getElementsByClassName('mainscore')[0].innerHTML = timeRemaining
+    document.getElementsByClassName('mainscore')[0].innerHTML = Math.abs(timeRemaining)
     timeRemaining -= 1
+}
+function updateScore() {
+    document.getElementsByClassName('attempts')[0].innerHTML = 'Attempt '+attempts+' of '+maxAttempts
+    document.getElementsByClassName('target')[0].innerHTML = 'Max Score: '+targetScore
+    document.getElementsByClassName('score')[0].innerHTML = 'Current Score: '+score
 }
 function stopClock() {
     clearInterval(clock);
+    attempts += 1
+    score += Math.abs(timeRemaining);
+    while (x < 50000) {
+        console.log(x);
+        x++;
+    }
+    x = 0
+    timeRemaining = 500
+    clock = setInterval(tickClock, 10)
+
+
 }
 
 document.addEventListener("click", stopClock);
